285c7851d484aebb6ce11c32f033198862957390
NackUn <king4521@naver.com>
2021-12-19 15:57:47 +0900

    FormatUtils 코드 정리

--- app/src/main/java/com/nackun/meetchecker/presentation/util/FormatUtils.kt
+++ app/src/main/java/com/nackun/meetchecker/presentation/util/FormatUtils.kt
@@ -4,9 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun Date.toStringYMD(): String {
-    val format = SimpleDateFormat(FORMAT_Y_M_D, Locale.getDefault())
-    return format.format(this)
-}
+fun Date.toStringYMD(): String =
+    SimpleDateFormat(FORMAT_Y_M_D, Locale.getDefault()).format(this)
 
-const val FORMAT_Y_M_D = "yyyy-MM-dd"
+private const val FORMAT_Y_M_D = "yyyy-MM-dd"
