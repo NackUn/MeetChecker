6068acffc327319e4f360673891ea35b52476048
NackUn <king4521@naver.com>
2021-12-22 00:14:56 +0900

    flipper 설정

--- app/src/main/java/com/nackun/meetchecker/MeetCheckApplication.kt
+++ app/src/main/java/com/nackun/meetchecker/MeetCheckApplication.kt
@@ -2,11 +2,33 @@ package com.nackun.meetchecker
 
 import android.app.Application
 import com.airbnb.mvrx.Mavericks
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
 
 class MeetCheckApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        initFlipper()
+        initMavericks()
+    }
+
+    private fun initFlipper() {
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(DatabasesFlipperPlugin(this))
+            client.start()
+        }
+    }
+
+    private fun initMavericks() {
         Mavericks.initialize(this)
     }
 }
