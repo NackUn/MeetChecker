40be07f9b58d199f098ef5faeb317658355b6fa3
NackUn <king4521@naver.com>
2022-01-16 21:44:57 +0900

    noto sans 폰트 추가

--- app/src/main/java/com/nackun/meetchecker/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/nackun/meetchecker/presentation/ui/theme/Type.kt
@@ -2,14 +2,21 @@ package com.nackun.meetchecker.presentation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.nackun.meetchecker.R
+
+val fonts = FontFamily(
+    Font(R.font.noto_sans_kr_regular),
+    Font(R.font.noto_sans_kr_bold, weight = FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
