46fa93cf4b5ceb54d9501ec5c479e38dde36351c
NackUn <king4521@naver.com>
2022-03-04 20:47:01 +0900

    test: ViewModelTest MvRxRule 추가

--- app/src/test/java/com/nackun/meetchecker/presentation/viewmodel/ViewModelTest.kt
+++ app/src/test/java/com/nackun/meetchecker/presentation/viewmodel/ViewModelTest.kt
@@ -2,8 +2,8 @@ package com.nackun.meetchecker.presentation.viewmodel
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.nackun.meetchecker.MainCoroutineScopeRule
-import com.nackun.meetchecker.presentation.di.testAppModule
+import com.airbnb.mvrx.test.MvRxTestRule
+import com.nackun.meetchecker.di.testAppModule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.After
 import org.junit.Before
@@ -24,12 +24,11 @@ internal abstract class ViewModelTest : KoinTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val mvrxRule = MvRxTestRule()
+
     @Mock
     private lateinit var context: Application
-
-    @ExperimentalCoroutinesApi
-    @get:Rule
-    val coroutineScope = MainCoroutineScopeRule()
 
     @ExperimentalCoroutinesApi
     @Before
@@ -38,17 +37,14 @@ internal abstract class ViewModelTest : KoinTest {
             androidContext(context)
             modules(testAppModule)
         }
+        onBefore()
     }
+
+    protected abstract fun onBefore()
 
     @ExperimentalCoroutinesApi
     @After
     fun tearDown() {
         stopKoin()
     }
-
-//    protected fun <T> LiveData<T>.test(): LiveDataTestObserver<T> {
-//        val testObserver = LiveDataTestObserver<T>()
-//        observeForever(testObserver)
-//        return testObserver
-//    }
 }
